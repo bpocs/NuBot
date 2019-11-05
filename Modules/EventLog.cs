f1917c5ca912cc3e5d5f0b7bb5cd4c76217b176c
bpocs <57384567+bpocs@users.noreply.github.com>
2019-11-04 21:22:54 -0800

    Add files via upload

---
+++ Modules/EventLog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace NuBot.Modules
+{
+    public class EventLog
+    {
+        public async Task Welcome(SocketGuildUser user)
+        {
+            var client = Global.Client;
+            ulong channelID = Convert.ToUInt64(Config.bot.publicChannel);
+            var welcomeChannel = client.GetChannel(channelID) as SocketTextChannel;
+            var avatar = user.GetAvatarUrl();
+            if (avatar == null)
+            {
+                avatar = user.GetDefaultAvatarUrl();
+            }
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"{user.Username} has Joined the Server.", avatar);
+            embed.WithColor(new Color(114, 137, 218));
+            embed.WithCurrentTimestamp();
+            await welcomeChannel.SendMessageAsync("", false, embed.Build());
+        }
+        public async Task Farewell(SocketGuildUser user)
+        {
+            var client = Global.Client;
+            ulong channelID = Convert.ToUInt64(Config.bot.publicChannel);
+            var welcomeChannel = client.GetChannel(channelID) as SocketTextChannel;
+            var avatar = user.GetAvatarUrl();
+            if (avatar == null)
+            {
+                avatar = user.GetDefaultAvatarUrl();
+            }
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"{user.Username} has Left the Server.", avatar);
+            embed.WithColor(new Color(114, 137, 218));
+            embed.WithCurrentTimestamp();
+            await welcomeChannel.SendMessageAsync("", false, embed.Build());
+        }
+        public async Task RoleAddedMessage(IGuildUser user, IRole role)
+        {
+            var client = Global.Client;
+            ulong channelID = Convert.ToUInt64(Config.bot.modChannel);
+            var welcomeChannel = client.GetChannel(channelID) as SocketTextChannel;
+            var avatar = user.GetAvatarUrl();
+            if (avatar == null)
+            {
+                avatar = user.GetDefaultAvatarUrl();
+            }
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"{user.Username} assigned themselves the \"{role}\" role. (ReactionRoles)", avatar);
+            embed.WithColor(new Color(114, 137, 218));
+            embed.WithCurrentTimestamp();
+            await welcomeChannel.SendMessageAsync("", false, embed.Build());
+        }
+        public async Task RoleRemovedMessage(IGuildUser user, IRole role)
+        {
+            var client = Global.Client;
+            ulong channelID = Convert.ToUInt64(Config.bot.modChannel);
+            var welcomeChannel = client.GetChannel(channelID) as SocketTextChannel;
+            var avatar = user.GetAvatarUrl();
+            if (avatar == null)
+            {
+                avatar = user.GetDefaultAvatarUrl();
+            }
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"{user.Username} retired from the \"{role}\" role. (ReactionRoles)", avatar);
+            embed.WithColor(new Color(114, 137, 218));
+            embed.WithCurrentTimestamp();
+            await welcomeChannel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}
---
+++ Modules/Misc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using NuBot.Core.UserAccounts;
+
+namespace NuBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        readonly string zws = "\u200B";
+        [Command("nu")]
+        public async Task Nu()
+        {
+            await Context.Channel.SendMessageAsync("**\"All life begins with Nu and ends with Nu. This is the truth! This is my belief! ...At least for now.\"** \n        -The Mystery of Life, vol. 841, chapter 26");
+        }
+        [Command("level")]
+        public async Task MyStats()
+        {
+            SocketUser user = Context.User;
+            var avatar = user.GetAvatarUrl();
+            if (avatar == null)
+            {
+                avatar = user.GetDefaultAvatarUrl();
+            }
+            var account = UserAccounts.GetAccount(user);
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"Current Stats for {user.Username}", avatar);
+            embed.WithFooter($"{user.Username} is Level {account.LevelNumber} and has {account.XP} XP.");
+            embed.WithColor(new Color(114, 137, 218));
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+        //admin only
+        [Command("rolecall")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Rolecall()
+        {
+            var ffxiv = Emote.Parse("<:ffxiv:640804382681726976>");
+            var mhw = Emote.Parse("<:mhw:641039265685307413>");
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Role Selection");
+            embed.WithDescription($"Please React here to unlock the desired Roles for this server: \n {zws}");
+            embed.AddField("<:ffxiv:640804382681726976> @FFXIV", $"Gives access to \"FFXIV\" Category \n {zws}");
+            embed.AddField("<:mhw:641039265685307413> @Monster Hunter", $"Gives access to \"Monster Hunter\" Category \n {zws}");
+            embed.WithFooter("Removing your reaction will also remove your Role");
+            embed.WithColor(new Color(114, 137, 218));
+            var reactionmessage = await Context.Channel.SendMessageAsync("", false, embed.Build());
+
+            await reactionmessage.AddReactionAsync(ffxiv);
+            await reactionmessage.AddReactionAsync(mhw);
+        }
+    }
+}
