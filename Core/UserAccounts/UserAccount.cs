c12c8320fbd449ccdae788a731f6b0bbee38b50b
bpocs <57384567+bpocs@users.noreply.github.com>
2019-11-04 21:25:05 -0800

    Add files via upload

---
+++ Core/UserAccounts/UserAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NuBot.Core.UserAccounts
+{
+    public class UserAccount
+    {
+        public ulong ID { get; set; }
+        public ulong XP { get; set; }
+
+        public uint LevelNumber
+        {
+            get
+            {
+                return (uint)Math.Sqrt(XP / 50);
+            }
+        }
+    }
+}
