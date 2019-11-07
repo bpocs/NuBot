e8591f49657ee8c48f0ae992d9d32c3dc7d7efe2
bpocs <57384567+bpocs@users.noreply.github.com>
2019-11-06 16:08:03 -0800

    Update Misc.cs

--- Modules/Misc.cs
+++ Modules/Misc.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -38,19 +38,34 @@ namespace NuBot.Modules
         public async Task Rolecall()
         {
             var ffxiv = Emote.Parse("<:ffxiv:640804382681726976>");
+            var gtav = Emote.Parse("<:gtav:641739856631955481>");
             var mhw = Emote.Parse("<:mhw:641039265685307413>");
+            var lol = Emote.Parse("<:lol:641744315525103687>");
+            var fps = Emote.Parse("<:fps:641740774316310528>");
+            var ffbe = Emote.Parse("<:ffbe:641741179293007913>");
+            var poke = Emote.Parse("<:poke:641742044569534484>");
 
             var embed = new EmbedBuilder();
             embed.WithTitle("Role Selection");
             embed.WithDescription($"Please React here to unlock the desired Roles for this server: \n {zws}");
-            embed.AddField("<:ffxiv:640804382681726976> @FFXIV", $"Gives access to \"FFXIV\" Category \n {zws}");
-            embed.AddField("<:mhw:641039265685307413> @Monster Hunter", $"Gives access to \"Monster Hunter\" Category \n {zws}");
-            embed.WithFooter("Removing your reaction will also remove your Role");
+            embed.AddField("<:ffxiv:640804382681726976> @FFXIV", "Gives access to the \"FFXIV\" Category");
+            embed.AddField("<:gtav:641739856631955481> @GTAV", "Gives access to the \"GTAV\" Category");
+            embed.AddField("<:mhw:641039265685307413> @Monster Hunter", "Gives access to the \"Monster Hunter\" Category");
+            embed.AddField("<:lol:641744315525103687> @League of Legends", "Gives access to the \"League of Legends\" Category");
+            embed.AddField("<:fps:641740774316310528> @FPS Junkie", "Gives access to the \"FPS Junky\" Category");
+            embed.AddField("<:ffbe:641741179293007913> @Brave Exvius", "Gives access to the \"Brave Exvius\" Category");
+            embed.AddField("<:poke:641742044569534484> @Pokemon Go", "Gives access to the \"Pokemon Go\" Category");
+            embed.WithFooter("Removing your reaction will also remove your Role. FFXIV FC Members and Static Raid members should ping a moderator for assigning additional roles.");
             embed.WithColor(new Color(114, 137, 218));
             var reactionmessage = await Context.Channel.SendMessageAsync("", false, embed.Build());
 
             await reactionmessage.AddReactionAsync(ffxiv);
+            await reactionmessage.AddReactionAsync(gtav);
             await reactionmessage.AddReactionAsync(mhw);
+            await reactionmessage.AddReactionAsync(lol);
+            await reactionmessage.AddReactionAsync(fps);
+            await reactionmessage.AddReactionAsync(ffbe);
+            await reactionmessage.AddReactionAsync(poke);
         }
     }
 }
